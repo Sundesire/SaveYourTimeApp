30c690029dd4b27755d4164c0e756460e780c152
Ivan Babushkin <zet6666@gmail.com>
2019-12-26 08:11:48 +0300

    Initial Commit

---
+++ SimpleYourTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleYourTime
+//
+//  Created by Иван Бабушкин on 26.12.2019.
+//  Copyright © 2019 Ivan Babushkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
